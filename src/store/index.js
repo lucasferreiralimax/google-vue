717e3ca5ce6d6faf504a497ef3d6e90669e1fd65
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-13 00:40:59 -0800

    adjustment darkmode with store state

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    darkmode: false,
   },
   mutations: {
+    updateDarkmode (state, value) {
+      state.darkmode = value
+    },
   },
   actions: {
   },
