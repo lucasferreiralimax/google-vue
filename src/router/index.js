8533dc6e9efe0c0316d1c47dda333e3aed072851
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-12 21:52:52 -0800

    config routes with pages

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/config',
+    name: 'Config',
+    component: () => import(/* webpackChunkName: "config" */ '../views/Config.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
   }
 ]
 
