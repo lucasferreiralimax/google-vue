a6fb6244cb0d51e49c4af25fe52e098fb3219fcb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-18 23:47:54 -0700

    home lazyloading component

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
   },
   {
     path: '/config',
