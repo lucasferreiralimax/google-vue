683d294d257aa0801e783b92521f5ff4006a9ef8
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-26 21:48:53 -0300

    test: unit valid components and url in Config

--- tests/unit/views/Config.spec.js
+++ tests/unit/views/Config.spec.js
@@ -1,11 +1,26 @@
 import { shallowMount } from '@vue/test-utils'
 
 import Config from '@/views/Config.vue'
+import Darkmode from '@/components/Darkmode'
+import Profile from '@/components/Profile'
 
 const wrapper = shallowMount(Config)
 
 describe('Config', () => {
   it('is Config a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
+  })
+  it('in Config a component Darkmode', () => {
+    expect(wrapper.getComponent(Darkmode).exists()).toBeTruthy()
+  })
+  it('in Config a component Profile', () => {
+    expect(wrapper.getComponent(Profile).exists()).toBeTruthy()
+  })
+  it('valid url for github google-vue', () => {
+    expect(
+      wrapper
+        .find('a')
+        .attributes('href')
+      ).toBe('https://github.com/lucasferreiralimax/google-vue')
   })
 })
