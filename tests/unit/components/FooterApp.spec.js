9fed0d36f281640786ef47afce5025ef19b464ea
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-26 22:08:56 -0300

    test: unit valid component in FooterApp, links and texts

--- tests/unit/components/FooterApp.spec.js
+++ tests/unit/components/FooterApp.spec.js
@@ -1,6 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 
 import FooterApp from '@/components/FooterApp.vue'
+import Darkmode from '@/components/Darkmode'
 import router from '@/router'
 
 const wrapper = shallowMount(FooterApp, {
@@ -10,8 +11,24 @@ const wrapper = shallowMount(FooterApp, {
 })
 
 describe('FooterApp', () => {
-  it('is FooterApp a vue instance', async () => {
+  beforeAll(async () => {
     await router.isReady()
+  })
+  it('is FooterApp a vue instance', async () => {
     expect(wrapper.exists()).toBeTruthy()
+  })
+  it('in FooterApp a component Darkmode', () => {
+    expect(wrapper.getComponent(Darkmode).exists()).toBeTruthy()
+  })
+  it('in FooterApp with text Brasil and svg', () => {
+    expect(wrapper.find('.brasil svg').exists()).toBeTruthy()
+    expect(wrapper.find('.brasil').text()).toBe('Brasil')
+  })
+  it('valid url for github @lucasferreiralimax', () => {
+    expect(
+      wrapper
+        .find('a:nth-of-type(1)')
+        .attributes('href')
+      ).toBe('https://github.com/lucasferreiralimax')
   })
 })
