abbdb76e68789bb3081ef877e1d8e6b965f51614
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-04-27 19:33:59 -0700

    adjustment translate with useHook

--- src/components/Darkmode.vue
+++ src/components/Darkmode.vue
@@ -5,7 +5,7 @@
       <path v-else d="M9.353 2.939a1 1 0 01.22 1.08 8 8 0 0010.408 10.408 1 1 0 011.301 1.3A10.003 10.003 0 0112 22C6.477 22 2 17.523 2 12c0-4.207 2.598-7.805 6.273-9.282a1 1 0 011.08.22z"/>
     </svg>
     <div v-if="!type">
-      <span>{{ $t('text.darkmode') }}</span>
+      <span>{{ t('text.darkmode') }}</span>
       <input id="darkmode" type="checkbox" :checked="darkmode" />
     </div>
   </button>
@@ -14,12 +14,14 @@
 <script>
 import { computed  } from 'vue'
 import { useStore } from 'vuex'
+import { useI18n } from 'vue-i18n';
 
 export default {
   name: 'Darkmode',
   props: ['type'],
   setup() {
     const store = useStore()
+    const { t } = useI18n({ useScope: 'global' });
     const darkmode = computed(() => store.state.darkmode)
 
     dark(JSON.parse(localStorage.getItem("darkmode")))
@@ -62,7 +64,8 @@ export default {
     return {
       toggleMode,
       darkmode,
-      dark
+      dark,
+      t
     }
   }
 }
