12048ea7ae0ef170b980d248f141debb202e765b
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-04 20:48:46 -0300

    remove old serviceWork and add vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   server: {
     port: '9999'
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
