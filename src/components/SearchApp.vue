6fa185c305bb464505ed3a282fcd88b3c2138d2a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-14 19:32:23 -0700

    adjustment translate in search component

--- src/components/SearchApp.vue
+++ src/components/SearchApp.vue
@@ -26,27 +26,27 @@ function activeVoice () {
 <template>
   <section class="App-search">
     <label class="label-search">
-      Pesquise ou digite uma URL
+      {{ $t('search_component.search_label') }}
       <svg class="icon-search" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
-      <input class="App-search-input" type="text" placeholder="Pesquise no Google ou digite um URL" :value="search" @input="searchUpdate">
+      <input class="App-search-input" type="text" :placeholder="$t('search_component.search_label')" :value="search" @input="searchUpdate">
     </label>
-    <button type="button" class='btn clean' :class="{'active': search}" @click="searchClean" aria-label="Botão limpar campo de busca">
+    <button type="button" class='btn clean' :class="{'active': search}" @click="searchClean" :aria-label="$t('search_component.label.clean')">
       <svg class="icon-clean" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" fill="#999"></path></svg>
-      <span>Limpar</span>
+      <span>{{ $t('search_component.clean') }}</span>
     </button>
-    <button type="button" class="btn keyboard" @click="toggleKeyboard()" aria-label="Botão habilitar teclado virtual">
+    <button type="button" class="btn keyboard" @click="toggleKeyboard()" :aria-label="$t('search_component.label.keyboard')">
       <img src='../assets/keyboard.png' class="icon-keyboard" alt="Icone Teclado" width="19" height="11" />
-      <span>Teclado virtual</span>
+      <span>{{ $t('search_component.keyboard') }}</span>
     </button>
-    <button type="button" class="btn voice" @click="activeVoice()" aria-label="Botão habilitar reconhecimento de voz">
+    <button type="button" class="btn voice" @click="activeVoice()" :aria-label="$t('search_component.label.voice')">
       <svg class="icon-voice" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 15c1.66 0 3-1.31 3-2.97v-7.02c0-1.66-1.34-3.01-3-3.01s-3 1.34-3 3.01v7.02c0 1.66 1.34 2.97 3 2.97z" fill="#4285f4"></path><path d="m11 18.08h2v3.92h-2z" fill="#34a853"></path><path d="m7.05 16.87c-1.27-1.33-2.05-2.83-2.05-4.87h2c0 1.45 0.56 2.42 1.47 3.38v0.32l-1.15 1.18z" fill="#f4b400"></path><path d="m12 16.93a4.97 5.25 0 0 1 -3.54 -1.55l-1.41 1.49c1.26 1.34 3.02 2.13 4.95 2.13 3.87 0 6.99-2.92 6.99-7h-1.99c0 2.92-2.24 4.93-5 4.93z" fill="#ea4335"></path></svg>
-      <span>Buscar por voz</span>
+      <span>{{ $t('search_component.search_voice') }}</span>
     </button>
-    <button type="button" class="btn search" @click="searchGoogle()" aria-label="Botão pesquisar">
-      Pesquisa Google
+    <button type="button" class="btn search" @click="searchGoogle()" :aria-label="$t('search_component.label.search')">
+      {{ $t('search_component.search') }}
     </button>
-    <button type="button" class="btn lucky" aria-label="Botão pesquisar aleatório com sorte">
-      Estou com sorte
+    <button type="button" class="btn lucky" :aria-label="$t('search_component.label.lucky')">
+      {{ $t('search_component.lucky') }}
     </button>
   </section>
 </template>
